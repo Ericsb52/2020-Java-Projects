85fc7e07820c54c75cbf1eabd492287bc10e874c
ebroadbent <broadbent52@aol.com>
2019-09-04 14:10:56 -0600

    ch3 work with me

---
+++ example_projects/src/ebroadbent_example_projects/Hello_world.java
@@ -0,0 +1,29 @@
+
+package ebroadbent_example_projects;
+
+/**
+ * @author ebroadbent
+ * hello world
+ * 9/19
+ *
+ */
+
+public class Hello_world {
+
+	/**
+	 * 
+	 */
+	public Hello_world() {
+		System.out.println("hello world");
+		System.out.println("im sick of this program");
+		String hello = "hello";
+		System.out.println(hello);
+	}
+
+
+	public static void main(String[] args) {
+		new Hello_world();
+
+	}
+
+}
---
+++ identify_yourself/src/identify_yourself/Class_sched.java
@@ -0,0 +1,24 @@
+package identify_yourself;
+
+public class Class_sched {
+
+	public Class_sched() {
+		System.out.println("---------------------------------");
+		System.out.println("|First: Eric   | Last: Broadbent|");
+		System.out.println("|Period 1      | Python         |");
+		System.out.println("|Period 2      | Python         |");
+		System.out.println("|Period 4      | Python         |");
+		System.out.println("|Period 5      | Python         |");
+		System.out.println("|Period 6      | Mobile app     |");
+		System.out.println("|Period 7      | Mobile app     |");
+		System.out.println("|Period 9      | Java           |");
+		System.out.println("|Period 10     | Java           |");
+		System.out.println("---------------------------------");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Class_sched();
+	}
+
+}
